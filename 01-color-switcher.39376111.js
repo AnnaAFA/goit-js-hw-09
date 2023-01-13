45f690f710d1e632572e2b24e8a972b8ff68a5a4
AnnaAFA <109284238+AnnaAFA@users.noreply.github.com>
2023-01-13 14:17:14 +0000

    Deploying to gh-pages from @ AnnaAFA/goit-js-hw-09@448ea0746c414372bc169382ee5775a33fb763bd 🚀

---
+++ 01-color-switcher.39376111.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");console.log(e),t.addEventListener("click",(function(){t.setAttribute("disabled","true"),o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(o)}));let o=null;
+//# sourceMappingURL=01-color-switcher.39376111.js.map
