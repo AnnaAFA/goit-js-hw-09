448ea0746c414372bc169382ee5775a33fb763bd
AnnaAFA <anna.fedoruk111@gmail.com>
2023-01-13 16:16:52 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+// console.log(startBtn);
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(stopBtn);
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId = null;
+
+function onStartBtnClick() {
+  startBtn.setAttribute('disabled', 'true');
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  startBtn.removeAttribute('disabled');
+  clearInterval(intervalId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
