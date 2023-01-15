33dff0de86c45e3415453ebbd9de465dc6110888
AnnaAFA <anna.fedoruk111@gmail.com>
2023-01-15 14:57:45 +0200

    task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,15 +2,15 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const buttonStartRef = document.querySelector('button[data-start]');
+const btnStartRef = document.querySelector('button[data-start]');
 const dataPickerRef = document.querySelector('#datetime-picker');
-const daysRemainRef = document.querySelector('span[data-days]');
-const hoursRemainRef = document.querySelector('span[data-hours]');
-const minutesRemainRef = document.querySelector('span[data-minutes]');
-const secondsRemainRef = document.querySelector('span[data-seconds]');
+const daysRef = document.querySelector('span[data-days]');
+const hoursRef = document.querySelector('span[data-hours]');
+const minutesRef = document.querySelector('span[data-minutes]');
+const secondsRef = document.querySelector('span[data-seconds]');
 
-buttonStartRef.addEventListener('click', handleButtonStartClick);
-buttonStartRef.setAttribute('disabled', true);
+btnStartRef.addEventListener('click', onBtnStartClick);
+btnStartRef.setAttribute('disabled', true);
 
 const options = {
   enableTime: true,
@@ -21,7 +21,7 @@ const options = {
     if (new Date() >= selectedDates[0]) {
       return Notiflix.Report.warning('Please choose a date in the future');
     }
-    buttonStartRef.removeAttribute('disabled');
+    btnStartRef.removeAttribute('disabled');
   },
 };
 
@@ -29,8 +29,8 @@ const setFlatpickr = flatpickr(dataPickerRef, options);
 
 let intervalId = null;
 
-function handleButtonStartClick() {
-  buttonStartRef.setAttribute('disabled', true);
+function onBtnStartClick() {
+  btnStartRef.setAttribute('disabled', true);
 
   intervalId = setInterval(() => {
     const timeRemain = setFlatpickr.selectedDates[0].getTime() - Date.now();
@@ -39,26 +39,21 @@ function handleButtonStartClick() {
       dataPickerRef.setAttribute('disabled', '');
     } else {
       clearInterval(intervalId);
-      Notiflix.Report.info('Time is up!', '', 'OK');
+      Notiflix.Report.info('Time is over!');
       dataPickerRef.removeAttribute('disabled');
     }
   }, 1000);
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -69,8 +64,8 @@ function addLeadingZero(value) {
 }
 
 function renderTimer({ days, hours, minutes, seconds }) {
-  daysRemainRef.textContent = addLeadingZero(days);
-  hoursRemainRef.textContent = addLeadingZero(hours);
-  minutesRemainRef.textContent = addLeadingZero(minutes);
-  secondsRemainRef.textContent = addLeadingZero(seconds);
+  daysRef.textContent = addLeadingZero(days);
+  hoursRef.textContent = addLeadingZero(hours);
+  minutesRef.textContent = addLeadingZero(minutes);
+  secondsRef.textContent = addLeadingZero(seconds);
 }
